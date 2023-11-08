a9976874a0aceb845663fdd9882c578529c2f34f
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-11-08 14:36:56 +0200

    Commit

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 """Basic authentication module"""
 
+
+import base64
+import binascii
 from .auth import Auth
 
 
@@ -17,3 +20,17 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """Returns the decoded value of Base64 string"""
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded = base64.b64decode(base64_authorization_header,
+                                       validate=True)
+            return decoded.decode('utf-8')
+        except binascii.Error:
+            return None
