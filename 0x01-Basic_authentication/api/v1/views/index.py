b7c389bf34252b961f06b81a29512ce1de807b86
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-11-08 10:21:26 +0200

    Commit

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,12 @@ def unauthorized_endpoint() -> str:
       - abort(401)
     """
     return abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden_endpoint() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+      - abort(403)
+    """
+    return abort(403)
