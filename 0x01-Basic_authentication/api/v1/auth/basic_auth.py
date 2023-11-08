7593091c6ddca697760241fa3588b64266d548e6
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-11-08 14:14:44 +0200

    Commit

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
 """Basic authentication module"""
 
+from werkzeug.datastructures import auth
 from .auth import Auth
 
 
 class BasicAuth(Auth):
     """Basic Authentication class"""
-    pass
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """return Base64 part of authorization header"""
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header[6:]
