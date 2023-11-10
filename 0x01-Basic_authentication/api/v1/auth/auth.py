c638fec41edf12353074b06737a642475da61426
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-11-11 01:59:42 +0200

    Commit

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,24 +11,12 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Required path"""
-        if path.endswith("/"):
-            path_with_slash = path
-        else:
-            path_with_slash = path + "/"
-        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+        if not excluded_paths or not path:
             return True
-        for exclude in excluded_paths:
-            if exclude.endswith("/"):
-                exclude_with_slash = exclude
-            else:
-                exclude_with_slash = exclude + "/"
-            last_segment = exclude_with_slash.split("/")[-1]
-            if last_segment.endswith("*"):
-                last_segment = last_segment[:-1] + "/"
-                if last_segment in path_with_slash:
-                    return False
-        if path_with_slash in excluded_paths or path in excluded_paths:
-            return False
+        path = path if path.endswith("/") else path + "/"
+        for excluded_path in excluded_paths:
+            if path.startswith(excluded_path.strip("*")):
+                return False
         return True
 
     def authorization_header(self, request=None) -> str:
