79d1ed842b848cefc139ebb78b1503f941f1a8fe
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-11-11 07:56:50 +0200

    Commit

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python3
 """Module contains SessionAuth class"""
 
+from typing import Dict
+import uuid
+
 from .auth import Auth
 
 
 class SessionAuth(Auth):
     """Session Authentication class"""
-    pass
+
+    user_id_by_session_id: Dict = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Create session ID for a user_id"""
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
