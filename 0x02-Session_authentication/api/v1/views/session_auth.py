60b5a4b98a908aff103e96db2b5d4f76dab00e5f
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-11-12 01:02:18 +0200

    Commit

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -4,6 +4,7 @@
 from os import getenv
 from flask import make_response, request, jsonify, abort
 from api.v1.views import app_views
+from api.v1.app import app
 from models.user import User
 
 
@@ -32,7 +33,7 @@ def session_authentication():
     return jsonify({"error": "wrong password"}), 401
 
 
-@app_views.route("/auth_session/logout",
+@app.route("/api/v1/auth_session/logout",
                  methods=["DELETE"], strict_slashes=False)
 def logout():
     """Destroy the session of logout"""
