71ece53accdbd0d2a5f9e58f3b7499f41b071d66
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-11-12 15:46:04 +0200

    Commit

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -25,14 +25,17 @@ class SessionExpAuth(SessionAuth):
 
     def user_id_for_session_id(self, session_id: str = None):
         """Return user id from session"""
-        if session_id is None:
-            return None
-        if session_id not in self.user_id_by_session_id:
-            return None
-        session_dict = self.user_id_by_session_id[session_id]
-        if self.session_duration <= 0 or "created_at" not in session_dict:
+        try:
+            if session_id is None:
+                return None
+            if session_id not in self.user_id_by_session_id:
+                return None
+            session_dict = self.user_id_by_session_id[session_id]
+            if self.session_duration <= 0 or "created_at" not in session_dict:
+                return session_dict.get("user_id")
+            created_at = session_dict.get("created_at")
+            if (created_at + timedelta(seconds=self.session_duration)) < datetime.now():
+                return None
             return session_dict.get("user_id")
-        created_at = session_dict.get("created_at")
-        if (created_at + timedelta(seconds=self.session_duration)) < datetime.now():
+        except Exception:
             return None
-        return session_dict.get("user_id")
