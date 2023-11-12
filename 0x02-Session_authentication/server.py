b8de2f35fac2093d8a4dddde951842adff530d7a
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-11-12 13:19:25 +0200

    Commit

---
+++ 0x02-Session_authentication/server.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" Main 3
+"""
+from flask import Flask, request, jsonify
+from api.v1.auth.session_auth import SessionAuth
+from models.user import User
+import os
+import json
+
+
+""" Create a user test """
+user = User()
+user.email = "usession2@hbtn.io"
+user.password = "pwdsession2"
+user.save()
+
+
+""" Create a session ID """
+session_auth = SessionAuth()
+session_id = session_auth.create_session(user.id)
+
+
+""" Save in file session ID and user ID """
+with open("session_id_hbtn2.json", "w") as file:
+    json.dump({ 'user_id': user.id, 'session_id': session_id }, file)
+
+
+""" Flask App """
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def root_path():
+    """ Root path
+    """
+    result_destroy = session_auth.destroy_session(request)
+    return jsonify({ 'destroy': result_destroy, 'user_id_by_session_id': session_auth.user_id_by_session_id })
+
+if __name__ == "__main__":
+    host = os.environ.get("API_HOST", "0.0.0.0")
+    port = os.environ.get("API_PORT", "5000")
+    app.run(host=host, port=port)
