6556b1d134a823a3ef7fef2a956870face3bf349
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-11-13 12:30:51 +0200

    Commit

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,64 @@
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.exc import InvalidRequestError,  NoResultFound
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+from user import Base
+from user import User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email=None, hashed_password=None):
+        """Add user method"""
+        if email is None or hashed_password is None:
+            return
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
+        self._session.commit()
+        return new_user
+
+    def find_user_by(self, **kwargs):
+        """Find user"""
+        try:
+            found = self._session.query(User).filter_by(**kwargs).first()
+            if found is None:
+                raise NoResultFound
+            return found
+        except NoResultFound:
+            raise NoResultFound
+        except InvalidRequestError:
+            raise InvalidRequestError
+
+    def update_user(self, user_id=None, **kwargs):
+        """Update user method"""
+        try:
+            user = self.find_user_by(id=user_id)
+            for key, value in kwargs.items():
+                if hasattr(User, key):
+                    setattr(user, key, value)
+                else:
+                    raise ValueError
+        except ValueError:
+            raise ValueError
