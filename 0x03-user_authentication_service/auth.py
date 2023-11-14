86a086357869bda6bf10c3e95d11fd31af288f6b
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-11-14 23:36:53 +0200

    Commit

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -99,8 +99,6 @@ class Auth:
         try:
             user = self._db.find_user_by(reset_token=reset_token)
         except NoResultFound:
-            user = None
-        if user is None:
             raise ValueError()
         hashed_pwd = _hash_password(password)
         self._db.update_user(
