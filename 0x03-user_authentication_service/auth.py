8afb24b474599f071e723ef2468dfc51bb53a6cd
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-11-14 23:18:59 +0200

    Commit

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -93,3 +93,15 @@ class Auth:
         reset_token = _generate_uuid()
         self._db.update_user(user.id, reset_token=reset_token)
         return reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Updates password using reset token"""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            if not user:
+                raise ValueError
+            hashed_pwd = _hash_password(password)
+            self._db.update_user(user.id, hashed_password=hashed_pwd)
+            return None
+        except ValueError as err:
+            raise err
