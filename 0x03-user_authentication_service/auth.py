e4e50b6ec99c5abf2dafab75e91a7e2e53fcbfca
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-11-14 20:43:17 +0200

    Commit

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,6 +2,8 @@
 """_hash_password module"""
 
 import bcrypt
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import User
 from db import DB
@@ -31,17 +33,18 @@ class Auth:
             raise err
         except Exception:
             hash_pwd = _hash_password(password)
-            new_user = self._db.add_user(email, str(hash_pwd))
+            new_user = self._db.add_user(email, hash_pwd)
             return new_user
 
     def valid_login(self, email: str, password: str) -> bool:
-        """Valid login"""
+        """
+        Checks if passed password matches encrypted password on database
+        """
         try:
             user = self._db.find_user_by(email=email)
-            stored_hashed_password = user.hashed_password.encode("utf-8")
-            provided_password = password.encode("utf-8")
-            return bcrypt.checkpw(provided_password, stored_hashed_password)
-        except ValueError:
-            return False
-        except Exception:
+            if bcrypt.checkpw(password.encode(), user.hashed_password):
+                return True
+            else:
+                return False
+        except (InvalidRequestError, NoResultFound):
             return False
