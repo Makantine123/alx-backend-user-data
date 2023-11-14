2a37a142830cbf6a4710db989d7c19e3c041918c
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-11-14 21:35:51 +0200

    Commit

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """App"""
 
-from flask import Flask, jsonify, request
+from flask import Flask, abort, jsonify, make_response, request
 from auth import Auth
 
 AUTH = Auth()
@@ -16,6 +16,7 @@ def index():
 
 @app.route("/users", methods=["POST"])
 def users():
+    """users"""
     try:
         email = request.form.get("email")
         password = request.form.get("password")
@@ -24,6 +25,22 @@ def users():
         return jsonify({"email": email, "message": "user created"})
     except Exception:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"])
+def login():
+    """login function"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    else:
+        session_id = AUTH.create_session(email)
+        mydict = jsonify({"email": email, "message": "logged in"})
+        response = make_response(mydict)
+        response.set_cookie("session_id", session_id)
+        return response
 
 
 if __name__ == "__main__":
