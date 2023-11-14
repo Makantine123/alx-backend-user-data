ce0ac8c50e194b77ae8fcb4177fe70dad56d2606
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-11-14 22:15:36 +0200

    Commit

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -46,14 +46,13 @@ def login():
 @app.route("/sessions", methods=["DELETE"])
 def logout():
     """logout function"""
-    session_id = request.form.get("session_id")
+    session_id = request.cookies.get("session_id")
     user = AUTH.get_user_from_session_id(session_id)
     if user:
-        user_id = user.id
-        AUTH.destroy_session(user_id)
+        AUTH.destroy_session(user.id)
         return redirect("/")
     else:
-        return abort(403)
+        abort(403)
 
 
 if __name__ == "__main__":
