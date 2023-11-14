5214688bbccefdaaa92dc29ce4e920210773fc82
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-11-14 22:42:59 +0200

    Commit

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -81,3 +81,13 @@ class Auth:
             return None
         except (InvalidRequestError, NoResultFound):
             pass
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Reset token"""
+        try:
+            user = self._db.find_user_by(email=email)
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
+        except ValueError:
+            raise ValueError
