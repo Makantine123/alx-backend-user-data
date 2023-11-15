c2be20afd5dc1389e66f5c84e880b5bf69f55a99
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-11-15 11:03:51 +0200

    Commit

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,10 +6,9 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str,
-                 seperator: str) -> str:
+def filter_datum(
+    fields: List[str], redaction: str, message: str, seperator: str
+        ) -> str:
     """filter_datum function"""
-    for field in fields:
-        message = re.sub(f'{field}=.*?{seperator}',
-                         f'{field}={redaction}{seperator}', message)
-    return message
+    extract, replace = (patterns["extract"], patterns["replace"])
+    return re.sub(extract(fields, seperator), replace(redaction), message)
