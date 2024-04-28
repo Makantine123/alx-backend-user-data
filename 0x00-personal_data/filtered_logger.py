b885793e4fc3b09a6d8c7e765538226eff7b01ff
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2024-04-28 20:38:09 +0200

    Commit

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -35,7 +35,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db():
+def get_db() -> mysql.connector.connection.MySQLConnection:
     """functioon"""
     username = environ.get("PERSONAL_DATA_DB_USERNAME", "root")
     password = environ.get("PERSONAL_DATA_DB_PASSWORD", "")
