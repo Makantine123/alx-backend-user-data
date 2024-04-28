f35a31804c6ece0f4e40fdbbbd0ea80ee2df3b03
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2024-04-28 16:19:05 +0200

    Commit

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,19 +3,7 @@
 
 
 import re
-from typing import List
 
+def filter_datum(fields, redaction, message, separator):
+    return re.sub(r'(?:(?<=^)|(?<=\{})){}=[^{}]*'.format(separator, '|'.join(fields), separator), '{}={}'.format(redaction, redaction), message)
 
-patterns = {
-    'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
-    'replace': lambda x: r'\g<field>={}'.format(x),
-}
-PII_FIELDS = ("name", "email", "phone", "ssn", "password")
-
-
-def filter_datum(
-    fields: List[str], redaction: str, message: str, seperator: str
-        ) -> str:
-    """filter_datum function"""
-    extract, replace = (patterns["extract"], patterns["replace"])
-    return re.sub(extract(fields, seperator), replace(redaction), message)
