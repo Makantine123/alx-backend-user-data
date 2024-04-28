2e1b9c3f8201d8ae01fda3d1c885cb973ac21864
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2024-04-28 16:56:43 +0200

    Commit

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """Module for personal data"""
 
-
+from typing import List
 import re
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """"Function"""
     for field in fields:
         message = re.sub(f'{field}=.*?{separator}',
